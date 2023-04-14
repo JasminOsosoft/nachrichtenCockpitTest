55aaeb149e2f20a019c509008124abef8a5ebca1
Jasmin Dedic <jasmin.dedic@ososoft.de>
2023-04-14 12:40:55 +0200

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["de/citti/nachrichtencockpit/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"decitti/nachrichtencockpit/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
